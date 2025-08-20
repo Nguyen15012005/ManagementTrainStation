503e56e50d33366be2aa58f06086a3ff844f04d2
unknown <23640731.nguyen@student.iuh.edu.vn>
2025-08-20 11:02:52 +0700

    first commit

---
+++ src/connectDB/DatabaseConnection.java
@@ -0,0 +1,48 @@
+package connectDB;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    private DatabaseConnection() {
+
+    }
+
+    public void connect() throws SQLException {
+        String url = "jdbc:sqlserver://localhost:1433;databasename=ManagementTrainStation";
+        String user = "Nguyen";
+        String password = "11092020";
+
+        connection = DriverManager.getConnection(url, user, password);
+    }
+
+    public void disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+}
